60d3e97fcd6d2598dbfac36ce06dea56dc9a9950
Sanchit Tandon <sanchittandon86@gmail.com>
2025-05-22 23:56:53 +0530

    datatypes summary

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,36 @@
+// *********************** OPERATIONS *********************** //
+
+// let value = 3
+// let value2 = -value
+// console.log(value2); // -3 // unary operator
+
+// console.log(2+2); // 4 // binary operator // addition operator
+// console.log(2-2); // 0 // binary operator // subtraction operator
+// console.log(2*2); // 4 // binary operator // multiplication operator
+// console.log(2/2); // 1 // binary operator // division operator
+// console.log(2**2); // 4 // binary operator // exponentiation operator
+// console.log(2%2); // 0 // binary operator // remainder operator
+
+// let str1 = "Hello";
+// let str2 = "World";
+// let str3 = str1 + " " + str2; // concatenation operator
+// console.log(str3); // Hello World
+
+// console.log("1" + 1); // 11 // string concatenation
+// console.log(1 + "1"); // 11 // string concatenation
+// console.log(1 + 1 + "1"); // 21 // string concatenation
+// console.log(1 + 1 + 1); // 3 // number addition
+// console.log(1 + 1 + "1" + 1); // 211 // string concatenation
+// console.log(1 + 1 + "1" - 1); // 20 // string concatenation
+// console.log(1 + 1 + "1" - 1 + 1); // 21 // string concatenation
+// console.log(1 + 1 + "1" - 1 + 1 - 1); // 20 // string concatenation
+// console.log(1 + 1 + "1" - 1 + 1 - 1 + 1); // 21 // string concatenation
+// console.log(1 + 1 + "1" - 1 + 1 - 1 + 1 - 1); // 20 // string concatenation
+
+
+// let gameScore = 100;
+// // gameScore++; // increment operator 101
+// // ++gameScore; // increment operator 101
+// console.log(gameScore); // 101
+// // gameScore--; // decrement operator 99
+// // gameScore += 10; // addition assignment operator 110

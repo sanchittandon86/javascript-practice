bbcc2177968a50f91d1ba893403d06d712fffd0e
sanchittandon86 <134963704+sanchittandon86@users.noreply.github.com>
2025-05-24 21:40:03 +0530

    Update 02_datatypes.js

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,5 +1,13 @@
 // Data Types in JavaScript
 
+// 1. Primitive Data Types -> 7
+// - Number -> for numbers (integers, floats)
+// - BigInt (ES11) -> for large integers
+// - String -> for text (single/double quotes)
+// - Boolean -> for true/false
+// - Null -> for standalone value
+// - Undefined -> for absence of value
+// - Symbol (ES6) -> for unique identifiers
 
 // 2. Reference Data Types
 // - Object
@@ -7,3 +15,13 @@
 // - Function
 
 "use strict"; // treat all JS code as newer version
+
+// alert();
+// alert("Hello World"); // show alert box will yhrow error in strict mode
+
+console.log(typeof undefined); // undefined is a type in JS
+console.log(typeof 0); // 0 is a number in JS
+console.log(typeof 1.5); // 1.5 is a number in JS
+console.log(typeof null); // null is an object in JS
+console.log(typeof true); // true is a boolean in JS
+console.log(typeof "Hello World"); // "Hello World" is a string in JS
